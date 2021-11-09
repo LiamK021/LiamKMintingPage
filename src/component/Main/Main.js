f69b1ceee26b6a8ab68342cf0233e358ab57d150
seanpen00 <seanpen00@gmail.com>
2021-11-09 17:34:45 -0500

    added

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -6,15 +6,22 @@ import * as s from "../../styles/globalStyles";
 import styled from "styled-components";
 import i1 from "../../assets/images/hidden.gif";
 import newbanner from "../../assets/images/logo.png"
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 import "./Main.css";
 =======
+=======
+>>>>>>> parent of b03be80 (update the button)
 
 export const StyledButton = styled.button`
   padding: 10px;
   border-radius: 50px;
   border: none;
+<<<<<<< HEAD
   background-color: "blue";
+=======
+  background-color: #ffffff;
+>>>>>>> parent of b03be80 (update the button)
   padding: 10px;
   padding-left: 15px;
   padding-right: 15px;
@@ -31,7 +38,10 @@ export const StyledButton = styled.button`
     -moz-box-shadow: none;
   }
 `;
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of b03be80 (update the button)
 
 export const ResponsiveWrapper = styled.div`
   display: flex;
@@ -57,57 +67,58 @@ export const StyledImg = styled.img`
 `;
 
 function Main() {
-    const dispatch = useDispatch();
-    const blockchain = useSelector((state) => state.blockchain);
-    const data = useSelector((state) => state.data);
-    const [feedback, setFeedback] = useState("");
-    const [claimingNft, setClaimingNft] = useState(false);
-    const claimNFTs = (_amount) => {
-        _amount = document.getElementById("inputBox").value;
-        if (_amount <= 0) {
-            return;
-        }
-        setFeedback("Minting your Official BooCrew NFT...");
-        setClaimingNft(true);
-        blockchain.smartContract.methods
-            .mint(blockchain.account, _amount)
-            // ********
-            // You can change the line above to
-            // .whiteListMint(blockchain.account, _amount) if you want only whitelisted
-            // users to be able to mint through your website!
-            // And after you're done with whitelisted users buying from your website,
-            // You can switch it back to .mint(blockchain.account, _amount).
-            // ********
-            .send({
-                gasLimit: 285000 * _amount,
-                to: "0x8815e06FC5b57Bd4d5590977a697582f19d2330e", // the address of your contract
-                from: blockchain.account,
-                value: blockchain.web3.utils.toWei((0.035 * _amount).toString(), "ether"),
-            })
-            .once("error", (err) => {
-                console.log(err);
-                setFeedback("Sorry, something went wrong. Check your transaction on Etherscan to find out what happened!");
-                setClaimingNft(false);
-            })
-            .then((receipt) => {
-                setFeedback(
-                    "Your BooCrew NFT has been successfully minted!"
-                );
-                setClaimingNft(false);
-                dispatch(fetchData(blockchain.account));
-            });
-    };
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [feedback, setFeedback] = useState("");
+  const [claimingNft, setClaimingNft] = useState(false);
+  const claimNFTs = (_amount) => {
+    _amount = document.getElementById("inputBox").value;
+    if (_amount <= 0) {
+      return;
+    }
+    setFeedback("Minting your Official BooCrew NFT...");
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _amount)
+      // ********
+      // You can change the line above to
+      // .whiteListMint(blockchain.account, _amount) if you want only whitelisted
+      // users to be able to mint through your website!
+      // And after you're done with whitelisted users buying from your website,
+      // You can switch it back to .mint(blockchain.account, _amount).
+      // ********
+      .send({
+        gasLimit: 285000 * _amount,
+        to: "0x8815e06FC5b57Bd4d5590977a697582f19d2330e", // the address of your contract
+        from: blockchain.account,
+        value: blockchain.web3.utils.toWei((0.035 * _amount).toString(), "ether"),
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong. Check your transaction on Etherscan to find out what happened!");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        setFeedback(
+          "Your BooCrew NFT has been successfully minted!"
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
 
-    const getData = () => {
-        if (blockchain.account !== "" && blockchain.smartContract !== null) {
-            dispatch(fetchData(blockchain.account));
-        }
-    };
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
 
-    useEffect(() => {
-        getData();
-    }, [blockchain.account]);
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
 
+<<<<<<< HEAD
 <<<<<<< Updated upstream
     return (
         <div style={{ backgroundColor: "var(--black)", fontSize: 40 }}>
@@ -236,6 +247,11 @@ function Main() {
   return (
     <s.Screen style={{ backgroundColor: "var(--black)", fontSize: 40 }}>
       <s.Container flex={1} ai={"center"} style={{ padding: 24, backgroundColor: "white"}}>
+=======
+  return (
+    <s.Screen style={{ backgroundColor: "var(--black)", fontSize: 40 }}>
+      <s.Container flex={1} ai={"center"} style={{ padding: 24, backgroundColor: "#FFA537"}}>
+>>>>>>> parent of b03be80 (update the button)
         <s.TextTitle
           style={{ display: "flex", flexDirection: "row", fontSize: 12, fontWeight: "bold", paddingRight: 10, margin: 0, 
           borderStyle: "solid", borderColor: "black", borderWidth: 0,
@@ -277,7 +293,11 @@ function Main() {
             jc={"center"}
             ai={"center"}
             style={{ 
+<<<<<<< HEAD
               backgroundColor: "white", 
+=======
+              backgroundColor: "#FFA537", 
+>>>>>>> parent of b03be80 (update the button)
               padding: 24,
               paddingTop: 0,
               borderStyle: "solid", 
@@ -291,11 +311,129 @@ function Main() {
                 <s.TextTitle style={{ textAlign: "center" }}>
                   The sale has ended.
                 </s.TextTitle>
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of b03be80 (update the button)
                 <s.SpacerSmall />
-            </div>
-        </div>
-    );
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  Dont worry, you're not missing out! You can still get Boo Crew NFTs on{" "}
+                  <a
+                    // target={"_blank"}
+                    href={"https://testnets.opensea.io/collection/rinkeby-doodlenauts"}
+                  >
+                    Opensea.io
+                  </a>
+                </s.TextDescription>
+              </>
+            ) : (
+              <>
+                {/* <s.TextTitle style={{ textAlign: "center", fontSize: 30 }}>
+                  1 DOODL costs .01 ETH.
+                </s.TextTitle>
+                <s.SpacerXSmall />
+                <s.TextDescription style={{ textAlign: "center", fontSize: 30 }}>
+                  Excluding gas fees.
+                </s.TextDescription>
+                <s.SpacerSmall /> */}
+                <s.TextDescription style={{ textAlign: "center", fontSize: 40 }}>
+                  {feedback}
+                </s.TextDescription>
+                {/* <s.SpacerMedium /> */}
+                {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                  <s.Container ai={"center"} jc={"center"}>
+                    <s.TextDescription style={{ textAlign: "center", fontSize: 80, marginBottom: 0 }}>
+                      Connect to the Boo Crew NFT Minter!
+                    </s.TextDescription>
+                    <s.SpacerSmall />
+                    <StyledButton
+                      style={{fontFamily: "coder"}}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(connect());
+                        getData();
+                      }}
+                    >
+                      CONNECT
+                    </StyledButton>
+                    <s.SpacerLarge />
+                    {/* <s.TextDescription style={{textAlign: "center", fontSize: 30, marginBottom: 0, paddingBottom: 0}}>
+                      <a href="https://google.com">Boo Crew NFT Smart Contract</a>
+                    </s.TextDescription> */}
+                    {blockchain.errorMsg !== "" ? (
+                      <>
+                        <s.SpacerSmall />
+                        <s.TextDescription style={{ textAlign: "center", fontSize: 50}}>
+                          {blockchain.errorMsg}
+                        </s.TextDescription>
+                      </>
+                    ) : null}
+                  </s.Container>
+                ) : (
+                  <s.Container ai={"center"} jc={"center"} fd={"row"} style={{marginTop: 0, paddingTop: 0}}>
+                    <form>
+                    I want <input 
+                    id="inputBox"
+                    placeholder="#" 
+                    type="number" 
+                    min="1" 
+                    max="100"
+                    style={{
+                      fontSize: 60,
+                      textAlign: "center",
+                      backgroundColor: "#FFA537",
+                      borderWidth: 4,
+                      borderColor: "black",
+                      borderStyle: "solid",
+                      borderRadius: 40,
+                      paddingRight: 10,
+                      // marginBottom: 20,
+                      // paddingLeft: 0,
+                      // marginLeft: 0,
+                      width: 80,
+                      fontFamily: "coder",}}
+                    /> Ghosts
+                    </form>
+                    <s.SpacerSmall/>
+                    <StyledButton
+                     style={{fontFamily: "coder"}}
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs(1);
+                        getData();
+                      }}
+                    >
+                      {claimingNft ? "BUSY" : "MINT"}
+                    </StyledButton>
+                  </s.Container>
+                )}
+              </>
+            )}
+          </s.Container>
+          </s.Container>
+          {/* <s.SpacerMedium /> */}
+        </ResponsiveWrapper>
+        <s.SpacerSmall />
+        <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
+          <s.TextDescription style={{ textAlign: "center", fontSize: 40}}>
+                      <a 
+                      href="https://rinkeby.etherscan.io/address/0x8815e06FC5b57Bd4d5590977a697582f19d2330e"
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                        fontSize: 20,
+                      }}
+                      >Boo Crew NFT Smart Contract</a>
+          </s.TextDescription>
+          <s.SpacerSmall />
+          {/* <s.TextDescription style={{ textAlign: "center", fontSize: 9 }}>
+          </s.TextDescription> */}
+        </s.Container>
+      </s.Container>
+    </s.Screen>
+  );
 }
 
 export default Main;
